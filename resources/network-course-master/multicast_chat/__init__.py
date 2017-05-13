49e7765051cb8d3d139d54456161348a018dc905
Roy Zohar <royzohar25@gmail.com>
2017-05-13 19:21:28 +0300

    Cut down resources a little

--- resources/network-course-master/multicast_chat/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-"""multicast_chat module."""
-
-
-# vim: expandtab tabstop=4 shiftwidth=4
--- resources/network-course-master/tcp_chat/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-# vim: expandtab tabstop=4 shiftwidth=4
